c8dddd766a97e748e7a19c9558dcf34569261835
bharani-byte <edubharani@gmail.com>
2025-09-11 14:47:25 +0530

    Q16 opp

---
+++ Q16  cprogram if else.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+    printf ("enter num 1:\n");
+    printf ("enter num 2:\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a==b)
+{ printf("enter num are equal");
+}
+else{
+	printf("entered number is not equal");
+}
+	return 0;
+}
