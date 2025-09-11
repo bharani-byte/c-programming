017223d8e2fb3944d824dbb7fce09af5a8d7ab75
bharani-byte <edubharani@gmail.com>
2025-09-11 14:42:04 +0530

    Q9 opp

---
+++ Q9 .to print the sum of all digits without last digit.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+    printf ("enter num 1:\n",a);
+    printf ("enter num 2:\n",b);
+    printf ("enter num 3:\n",c);
+    scanf("%d",&a);
+    scanf("%d",&b);
+	return 0;
+}
