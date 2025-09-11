0b53f2a69b77fe9fe48f5db32495e687bbf44679
bharani-byte <edubharani@gmail.com>
2025-09-11 14:40:13 +0530

    q19 op

---
+++ c program Q19.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+ printf("enter the number:");
+ scanf("%d%d%d,&a,b,c");
+ printf("the greatest number is:%d\n",a);
+ printf("the greatest number is:%d\n",b);
+ printf("the greatest number is:%d\n",c);
+	return 0;	
+}
